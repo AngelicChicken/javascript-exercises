f5eef48b3fb1be4626bf8616a25e8061ffc18898
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-25 08:16:16 +0700

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const splited = text.split("");
+    const reversed = splited.toReversed();
+    const joined = reversed.join("");
 
+    return joined;
 };
 
 // Do not edit below this line
