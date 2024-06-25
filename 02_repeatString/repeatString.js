3a5565e4192b4e619a7b5bf5647dd4ace2d0a7b3
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-25 07:55:32 +0700

    passed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(kata, nomor) {
+    let stringToRepeat = kata;
+    let amountOfRepeat = nomor;
+    let timesRepeat = 0;
+    let endResult = '';
 
+    if(amountOfRepeat < 0){
+        return "ERROR"
+    }
+
+    while(timesRepeat < amountOfRepeat){
+        endResult += `${stringToRepeat}`;
+        timesRepeat++
+    }
+    return endResult;
 };
 
 // Do not edit below this line
