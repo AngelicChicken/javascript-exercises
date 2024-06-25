dc33ba45033a8b1d6ed98726b5165766cc199b87
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-06-25 09:58:50 +0700

    finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...toRemove) {
+    let variable = [];
+    let output = [];
+
+    toRemove.forEach(addToArray);
+    
+    for(let item of theArray){
+        if(variable.includes(item) == false){
+            output.push(item)
+        }
+    }
+
+    function addToArray(args){
+        variable.push(args);
+    }
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
