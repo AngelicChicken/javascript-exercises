b28a0a07eed6958c3f00072b78d44ea8bab5f428
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-04 10:27:24 +0700

    finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let noPunctuation = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let noSpace = noPunctuation.replace(/ /g, "");
 
+    let allLower = noSpace.toLowerCase();
+    console.log(allLower[allLower.length -1]);
+    console.log(allLower);
+    
+    let n = 0;
+    for(let i = allLower.length - 1; n < i; i--){
+        console.log(`i = ${i}`);
+        console.log(`n = ${n}`);
+        if(allLower[i] != allLower[n]){
+            console.log(allLower[i], allLower[n]);
+            return false
+        } else {
+        console.log(allLower[i], allLower[n]); 
+        n++;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
