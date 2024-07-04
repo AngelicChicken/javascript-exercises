dadc74a99039826d5d5f8dbd97c0eb08808cb932
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-04 09:06:34 +0700

    finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	result = arr.reduce((total, currentNum)=> {
+    return total + currentNum;
+  }, 0);
+
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  result = arr.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0){
+    return 1;
+  } else {
+    let i = 1;
+    let total = 1;
+    while(i <= num){
+      total *= i;
+      i++;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
