936f32e6118bb424b2af6bc725c8b528f9217060
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-05 02:06:30 +0700

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const age = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
 
+    return death - birth;
+};
+
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentPerson) => {
+        const old = age(oldest.yearOfBirth, oldest.yearOfDeath);
+        const current = age(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        return current > old? oldest = currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
