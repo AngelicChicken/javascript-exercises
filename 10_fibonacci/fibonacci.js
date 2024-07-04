ddaaf0783855962511eefc3c86fbfc58d09d6abc
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-05 01:01:29 +0700

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let arr = [1, 1];
+    index = parseInt(index);
+    let i = 0;
+    let counter = 2;
 
+    if(index == 0) return 0;
+    if(index < 0) return 'OOPS';
+
+    while(counter < index){
+        i = arr[arr.length-2] + arr[arr.length-1];
+        arr.push(i);
+        counter++
+    }
+
+    return arr[index - 1]
 };
 
 // Do not edit below this line
